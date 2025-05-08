f6aa4fc5a8e1e1cb9c9f6a9c38e986abd8243511
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2025-05-07 22:32:43 -0500

    metodos get

--- src/main/java/co/edu/javeriana/sv_users/Controller/EnfermeraController.java
+++ src/main/java/co/edu/javeriana/sv_users/Controller/EnfermeraController.java
@@ -3,16 +3,26 @@ package co.edu.javeriana.sv_users.Controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.javeriana.sv_users.DTO.UserDTO;
 import co.edu.javeriana.sv_users.Entity.Account;
 import co.edu.javeriana.sv_users.Entity.EnfermeraEntity;
+import co.edu.javeriana.sv_users.Entity.TipoIdentificacionEntity;
+import co.edu.javeriana.sv_users.Entity.TurnoEntity;
 import co.edu.javeriana.sv_users.Repository.EnfermeraRepository;
 import co.edu.javeriana.sv_users.Repository.RolRepository;
 import co.edu.javeriana.sv_users.Repository.TipoIdentificacionRepository;
@@ -61,6 +71,50 @@ public class EnfermeraController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("An unexpected error occurred: " + e.getMessage());
         }
+    }
+
+    // http://localhost:8080/enfermeras
+    @GetMapping("")
+    public ResponseEntity<?> getAllEnfermeras(@RequestParam(defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "0") int page) {
+        try {
+            Pageable pageable = PageRequest.of(page, limit);
+            Page<EnfermeraEntity> enfermerasPage = enfermeraService.getAllEnfermeras(pageable);
+
+            return ResponseEntity.ok(Map.of(
+                    "content", enfermerasPage.getContent(),
+                    "totalElements", enfermerasPage.getTotalElements()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body("An unexpected error occurred: " + e.getMessage());
+        }
+    }
+
+    // http://localhost:8080/enfermeras/1
+    @PutMapping("/{id}")
+    public ResponseEntity<EnfermeraEntity> actualizar(@PathVariable Long id, @RequestBody EnfermeraEntity enfermera) {
+        EnfermeraEntity existente = enfermeraRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No encontrada"));
+
+        TurnoEntity turno = turnoRepository.findByName(enfermera.getTurnoEntity().getName());
+        TipoIdentificacionEntity tipo = tipoIdentificacionRepository
+                .findByName(enfermera.getTipoIdentificacion().getName());
+
+        existente.setNombre(enfermera.getNombre());
+        existente.setApellido(enfermera.getApellido());
+        existente.setNumeroIdentificacion(enfermera.getNumeroIdentificacion());
+        existente.setTelefono(enfermera.getTelefono());
+        existente.setTurnoEntity(turno);
+        existente.setTipoIdentificacion(tipo);
+        existente.setDireccion(enfermera.getDireccion());
+        existente.setBarrio(enfermera.getBarrio());
+        existente.setEmail(enfermera.getEmail());
+        existente.setConjunto(enfermera.getConjunto());
+
+
+        enfermeraRepository.save(existente);
+        return ResponseEntity.ok(existente);
     }
 
 }
